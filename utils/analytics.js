19534a70332f257dc93fc19ef74633b4375ef945
Chris Ellis <ellis.cs@gmail.com>
2021-04-05 13:43:56 +0100

    feat: update gA

--- utils/analytics.js
+++ utils/analytics.js
@@ -2,7 +2,7 @@
 import ReactGA from 'react-ga'
 export const initGA = () => {
   // console.log("GA init");
-  ReactGA.initialize('G-VNLQFNK0TG')
+  ReactGA.initialize('UA-5447560-14')
 }
 export const logPageView = () => {
   // console.log(`Logging pageview for ${window.location.pathname}`);
